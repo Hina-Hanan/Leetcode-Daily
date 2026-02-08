a94d6bc7eb6d1065ccd425d7e39334c078d1f686
Hina Hanan <hinahanan2003@gmail.com>
2026-02-08 19:35:42 +0530

    Time: 4 ms (20.97%), Space: 22.5 MB (36.47%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -4,22 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeElements(self, head, val):
-        """
-        :type head: ListNode
-        :type val: int
-        :rtype: ListNode
-        """
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # dummy_head = ListNode(-1)
+        # dummy_head.next = head
         
-        dummy_head = ListNode(-1)
-        dummy_head.next = head
-        
-        current_node = dummy_head
-        while current_node.next != None:
-            if current_node.next.val == val:
-                current_node.next = current_node.next.next
-            else:
-                current_node = current_node.next
+        # current_node = dummy_head
+        # while current_node.next != None:
+        #     if current_node.next.val == val:
+        #         current_node.next = current_node.next.next
+        #     else:
+        #         current_node = current_node.next
                 
-        return dummy_head.next
-        
+        # return dummy_head.next
+        t=ListNode()
+        t.next=head
+        temp=t
+        while temp.next != None:
+            if temp.next.val==val:
+                temp.next=temp.next.next
+            else:
+               temp=temp.next
+        return t.next
